eb4ffa3926775cb0ac7110f4238cba7005011146
square <chenyu.wang@chiefclouds.com>
2019-04-12 17:43:26 +0800

    update mysql_connection with two query function

--- check_data_quality/cc/checkrn.py
+++ check_data_quality/cc/checkrn.py
@@ -4,6 +4,7 @@ import sys
 import os
 import codecs
 
+
 # gbk gb18030
 
 
@@ -11,11 +12,11 @@ def checkFile(filePath, readCode='utf_8_sig'):
     dir_name = os.path.dirname(filePath)
     new_file_name = os.path.splitext(os.path.basename(filePath))[0] \
                     + '_error.csv'
-    new_file_path = dir_name + os.sep+new_file_name
+    new_file_path = dir_name + os.sep + new_file_name
     print(new_file_path)
 
     with codecs.open(filePath, 'rb', readCode) as fr, \
-        codecs.open(new_file_path, 'w', readCode) as fw:
+            codecs.open(new_file_path, 'w', readCode) as fw:
 
         fw.write("start check " + filePath)
         i = 0
@@ -33,12 +34,12 @@ def checkFile(filePath, readCode='utf_8_sig'):
             if not line:
                 # 如果是空行，或者当前行最后一个字符不是双引号
                 error_line = error_line + 1
-                print(filePath+" line: "+str(i))
+                print(filePath + " line: " + str(i))
                 fw.write(filePath + '  line:' + str(i) + '\n')
                 print(line)
                 fw.write(line + '\n')
 
-            i = i+1
+            i = i + 1
         fw.write('Total error line: ' + str(error_line))
 
 
@@ -48,13 +49,13 @@ def readDir(dirPath, ext, readCode):
 
         for filename in files:
             if filename[-len(ext):] == ext:
-                checkFile(os.path.join(root, filename),readCode)
+                checkFile(os.path.join(root, filename), readCode)
         for dirname in dirs:
-            readDir(os.path.join(root, dirname),ext,readCode)
+            readDir(os.path.join(root, dirname), ext, readCode)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4 :
+    if len(sys.argv) != 4:
         print("usage: checkrn.py dirpath ext readcode")
         exit(1)
     readDir(sys.argv[1], sys.argv[2], sys.argv[3])
